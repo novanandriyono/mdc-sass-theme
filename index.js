982299e98a73b228760da0a7aae31ce9690d6c86
Novan Andriyono <evelynradtya@gmail.com>
2018-01-06 23:13:35 +0700

    add fs.chmod 0755 for folder and 0644 for file

--- index.js
+++ index.js
@@ -21,83 +21,118 @@ module.exports = function(options){
 		}
 	}
 
-function create(themename,pathsass){
-	var fs = require('fs');
-	fs.readFile(__dirname + '/dist/sass/new/index.scss', 'utf8', (err,txt) => {
-	  if(err){
-	  	console.log(err);
-	  }else{
-	  	fs.readdir(pathsass, (err,themeDir)=>{
-			if(err){
-		  		console.log("folder target no exist");
-		  	}else{
-		  		var themefolder =  pathsass + '/' + themename + '/';
-		  		var newfile = themefolder + '/index.scss';
-		  		if (!fs.existsSync(themefolder.toString())){
-		  			fs.mkdirSync(themefolder.toString());
-		  			fs.writeFile(newfile, txt, (err,file) => {
-	  					if(err){
-	  						console.log(err);
-	  					}else{
-	  						var mdcsasstheme = fs.readdirSync('./node_modules/mdc-sass/dist/theme/default');
-  							for(var i = 0, len = mdcsasstheme.length; i < len; i++){
-								var name = mdcsasstheme[i];
-								var filepath = './node_modules/mdc-sass/dist/theme/default/' + name;
-								var outputpath = themefolder + name;
-								var newtxt = fs.readFileSync(filepath, 'utf8');
-									fs.writeFile(outputpath, newtxt, (err,fileresult) => {
-									  if (err){
-									  	console.log(err);
-									  }else{
-									  	console.log(fileresult);
-									  };
-									});
-							}
-	  					}
-		  			});
-		  		}
-		  	}
+	function create(themename,pathsass){
+		var fs = require('fs');
+		fs.readFile(__dirname + '/dist/sass/new/index.scss', 'utf8', (err,txt) => {
+		  if(err){
+		  	console.log(err);
+		  }else{
+		  	fs.readdir(pathsass, (err,themeDir)=>{
+				if(err){
+			  		console.log("folder target no exist");
+			  	}else{
+			  		var themefolder =  pathsass + '/' + themename + '/';
+			  		var newfile = themefolder + '/index.scss';
+			  		if (!fs.existsSync(themefolder.toString())){
+			  			fs.mkdir(themefolder.toString(),(err)=>{
+			  				if(err){
+			  					console.log(err);
+			  				}else{
+			  					fs.writeFile(newfile, txt, (err) => {
+			  						if(err){
+			  							console.log(err);
+			  						}else{
+			  							var mdcsasstheme = fs.readdirSync('./node_modules/mdc-sass/dist/theme/default');
+			  							for(var i = 0, len = mdcsasstheme.length; i < len; i++){
+			  								var name = mdcsasstheme[i];
+			  								var filepath = './node_modules/mdc-sass/dist/theme/default/' + name;
+			  								var outputpath = themefolder + name;
+			  								var newtxt = fs.readFileSync(filepath, 'utf8');
+			  								fs.writeFile(outputpath, newtxt, (err) => {
+			  									if (err){
+			  										console.log(err);
+			  									}else{
+			  										fs.chmod(outputpath,0644,(err)=>{
+			  											if(err){
+			  												console.log(err);
+			  											}else{
+			  												console.log(outputpath);
+			  											}
+			  										});
+			  									};
+			  								});
+			  							}
+			  							fs.chmod(newfile,0644,(err)=>{
+			  								if(err){
+			  									console.log(err);
+			  								}else{
+			  									console.log(newfile);
+			  								}
+			  							});
+			  						}
+			  					});
+			  					fs.chmod(themefolder,0755,(err)=>{
+					  				if(err){
+					  					console.log(err);
+					  				}else{
+					  					console.log(themefolder);
+					  				}
+					  			});
+			  				}
+			  			});
+			  		}else{
+			  			console.log(themefolder, 'has exist');
+			  		}
+			  	}
+			});
+		  }
 		});
-	  }
-	});
-}
+	}
 
-function compile(indexsasspath,pathsass,pathcss){
-	var nodesass = require('node-sass');
-	var fs = require('fs');
-	var pathCss = pathcss + '/' + indexsasspath + '.css';
-	var pathSass = pathsass + '/' + indexsasspath + '/index.scss';
-	nodesass.render(
-		{
-			file: pathSass,
-			outFile:pathCss,
-		},
-		function (error, result) {
-			if (!error) {
-				fs.writeFile(pathCss, result.css, function (err,file) {
-					if (err) {
-						console.log(err);
-					} else {
-						var csso = require('csso');
-						var min = pathCss.replace(".css", ".min.css");
-						var css = fs.readFileSync(pathCss, 'utf8');
-						var minifiedCss = csso.minify(css).css;
-						fs.writeFile(min, minifiedCss, function (err) {
-							if (!err) {
-								console.log('done minify');
-							} else {
-								console.log('sass compiled ok but writing to file failed');
+	function compile(indexsasspath,pathsass,pathcss){
+		var nodesass = require('node-sass');
+		var fs = require('fs');
+		var pathCss = pathcss + '/' + indexsasspath + '.css';
+		var pathSass = pathsass + '/' + indexsasspath + '/index.scss';
+		nodesass.render(
+			{
+				file: pathSass,
+				outFile:pathCss,
+			},
+			function (error, result) {
+				if (!error) {
+					fs.writeFile(pathCss, result.css, function (err,file) {
+						if (err) {
+							console.log(err);
+						} else {
+							var csso = require('csso');
+							var min = pathCss.replace(".css", ".min.css");
+							var css = fs.readFileSync(pathCss, 'utf8');
+							var minifiedCss = csso.minify(css).css;
+							fs.writeFile(min, minifiedCss, function (err) {
+								if (!err) {
+									fs.chmod(min,0644,(err)=>{
+										if(err){
+										console.log(err);
+										}
+									});
+									console.log('done minify');
+								} else {
+									console.log(err);
+								}
+							});
+						}
+						fs.chmod(pathCss,0644,(err)=>{
+							if(err){
+								console.log(err);
 							}
 						});
-
-					}
-
-				});
-			} else {
-				console.log(error);
+					});
+				} else {
+					console.log(error);
+				}
 			}
-		}
-	);
-}
+		);
+	}
 
 };
