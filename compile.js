dfec42241c9da17fc79850c0e0560ffd162d85de
Novan Andriyono <evelynradtya@gmail.com>
2018-01-18 18:53:18 +0700

    fix some file

--- compile.js
+++ compile.js
@@ -1,32 +1,43 @@
 'use strict';
-const fs = require('fs');
-const path = require('path');
-const nodesass = require('node-sass');
-let name = process.argv[2];
-let sassPath = process.argv[3];
-let cssPath = process.argv[4];
-
+var fs = require('fs');
+var path = require('path');
+var nodesass = require('node-sass');
+var name = undefined;
+var sassPath = undefined;
+var cssPath = undefined;
+module.exports = compileTheme;
+if(process.argv.length === 5){
 if((typeof process.argv[2] != 'undefined') &&
 	(typeof process.argv[3] != 'undefined') &&
 	(typeof process.argv[4] != 'undefined')){
+		var name = process.argv[2];
+		var sassPath = process.argv[3];
+		var cssPath = process.argv[4];
 		return cac(name,sassPath,cssPath);
 	}else{
 		return console.log('please fill name and location for sass and css folder');
 	}
+}else{
+	// return compivarheme(name,sassPath,cssPath);
+}
+
+function compileTheme(name,sassPath,cssPath){
+	return cac(name,sassPath,cssPath);
+}
 
 function cac(name,sassPath,cssPath){
 
-	let result = new Array();
+	var result = new Array();
 
-	let sassPathName = sassPath + path.sep + name + path.sep + name + '.scss';
+	var sassPathName = sassPath + path.sep + name + path.sep + name + '.scss';
 
 	if(!fs.existsSync(sassPathName)){
-		let sassPathmsg = 'Notice :' + sassPathName + ' doesnt exists' + "\n";
+		var sassPathmsg = 'Notice :' + sassPathName + ' doesnt exists' + "\n";
 		result.push(sassPathmsg);
 	}
 
 	if(!fs.existsSync(cssPath)){
-		let cssPathmsg = 'Notice :' + cssPath + ' doesnt exists' + "\n";
+		var cssPathmsg = 'Notice :' + cssPath + ' doesnt exists' + "\n";
 		result.push(cssPathmsg);
 	}
 
@@ -55,13 +66,12 @@ function compile(name,sassPath,cssPath){
 					if (err) {
 						console.log(err);
 					} else {
-						const csso = require('csso');
-						let min = cssPath.replace(".css", ".min.css");
-						let css = result.css;
-						let minifiedCss = csso.minify(result.css).css;
+						var csso = require('csso');
+						var min = cssPath.replace(".css", ".min.css");
+						var minifiedCss = csso.minify(result.css).css;
 						fs.writeFile(min, minifiedCss, function (err) {
 							if (!err) {
-								let minMode = parseInt('0644',8);
+								var minMode = parseInt('0644',8);
 								fs.chmod(min,minMode,(err)=>{
 									if(err){
 									console.log(err);
@@ -73,7 +83,7 @@ function compile(name,sassPath,cssPath){
 							}
 						});
 					}
-					let pathCssMode = parseInt('0644',8);
+					var pathCssMode = parseInt('0644',8);
 					fs.chmod(cssPath,pathCssMode,(err)=>{
 						if(err){
 							console.log(err);
